76251401993eb317ee248dbf598e0a28af71d5a5
gustav <colbertcrypto@outlook.com>
2022-03-26 00:51:46 +0800

    interview problem solved

--- x/interview/client/cli/tx.go
+++ x/interview/client/cli/tx.go
@@ -60,3 +60,38 @@ func GetCmdLockUser() *cobra.Command {
 
 	return cmd
 }
+
+// GetCmdLockUser returns the interview module's transaction command.
+func GetCmdUnlockUser() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "unlock-user <user id>",
+		Short: "Unlock a user with given user id",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			txf := tx.NewFactoryCLI(cliCtx, cmd.Flags()).WithTxConfig(cliCtx.TxConfig).WithAccountRetriever(cliCtx.AccountRetriever)
+
+			from := cliCtx.GetFromAddress()
+			if err := txf.AccountRetriever().EnsureExists(cliCtx, from); err != nil {
+				return err
+			}
+
+			id, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgUnlockUser(from.String(), id)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxWithFactory(cliCtx, txf, msg)
+		},
+	}
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
